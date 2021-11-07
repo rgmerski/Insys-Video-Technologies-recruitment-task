d923b73b258f60d6cb7fc82e2a8b3b0e3da62515
Robert Gmerski <gmerek@live.com>
2021-11-07 13:55:09 +0100

    Implementing CRUD interfaces

---
+++ src/MovieLibrary.Data/IServicesMovie.cs
@@ -0,0 +1,17 @@
+﻿using MovieLibrary.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Data
+{
+    interface IServicesMovie<TEntity>
+        where TEntity : Movie
+    {
+        Task<TEntity> CreateAsync(TEntity entity);
+        Task<TEntity> ReadAsync(TEntity entity, bool tracking = true);
+        Task<TEntity> UpdateAsync(int id, TEntity updateEntity);
+        Task<TEntity> DeleteAsync(int id);
+    }
+}
